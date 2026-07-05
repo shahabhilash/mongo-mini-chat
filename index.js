bcb75e96ffdedfaf84a767b57556adfeb658a7f2
shahabhilash <abhilashshah2006@gmail.com>
2026-07-05 13:34:30 +0530

    feat: add edit chat feature

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require('./models/chat.js');
+const methodOverride = require('method-override');
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 main()
     .then(() => {
         console.log('Connected to MongoDB')
@@ -54,7 +57,24 @@ app.post("/chats",(req, res) => {
     newChat.save().then((res) =>
          {console.log("chat was saved")}).catch((err) => {console.log(err)});
     res.redirect("/chats");
-})
+});
+
+//edit route
+app.get("/chats/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat})
+});
+
+//update route
+app.put("/chats/:id", async (req, res) => {
+    let {id} = req.params;
+    let {msg: newMsg} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id, {msg: newMsg}, {new: true, runValidators: true});
+    console.log(updatedChat);
+    res.redirect("/chats");
+
+});
 
 app.get("/", (req, res) => {
     res.send("chl raha hai");
